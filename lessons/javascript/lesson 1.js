ec5268c552bb8d40b5fe869d7f714b5d4804eda5
ibrahimmd211 <ibbyd2345@gmail.com>
2023-04-08 19:18:22 +0100

    work

---
+++ lessons/javascript/lesson 1.js
@@ -0,0 +1,9 @@
+console.log("this is my first lesson")
+/*this is my first
+lesson of the day 
+this week*/
+// crazjdlnadnsal
+let age = 24
+const name = "ibrahim"
+
+console.log(`hello my name is ${name} and i am ${age} year old`)
